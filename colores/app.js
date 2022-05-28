2a8dee71e24618c66ac54400cb081b971e7e12b1
David <davidga205@gmail.com>
2022-05-28 11:36:32 -0500

    update

---
+++ colores/app.js
@@ -0,0 +1,13 @@
+const colors =["green", "red", "rgba(133,122,200)","#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click',function(){
+    const ramdomNumer = getRandomNumber;
+    document.body.style.background =colors[ramdomNumer];
+    color.textContent = colors[ramdomNumer];
+});
+
+function getRandomNumber(){
+    return Math.random()*colors.length;
+}
